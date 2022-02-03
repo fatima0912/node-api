346a9991063d2e0de9f561f3b45ec98aacd9d77d
Mujib <mujib517@gmail.com>
2022-02-03 08:46:21 +0530

    Post, put, Patch & Delete

--- controllers/bookCtrl.js
+++ controllers/bookCtrl.js
@@ -60,9 +60,61 @@ class BookCtrl {
     // res headers: content-type: application/xml
     // body
     create(req, res) {
-        console.log(req.body);
+        books.push(req.body);
         res.status(201);
         res.send("Created");
+    }
+
+    delete(req, res) {
+        const id = +req.params.id;
+        for (let i = 0; i < books.length; i++) {
+            // strict equality
+            if (books[i].id === id) {
+                books.splice(i, 1);
+                break;
+            }
+        }
+
+        res.status(204);
+        res.send();
+    }
+
+    update(req, res) {
+        if (!req.body.name || !req.body.price) {
+            res.status(400);
+            res.send("Bad request");
+        } else {
+            const id = +req.params.id;
+
+            for (let i = 0; i < books.length; i++) {
+                if (books[i].id === id) {
+                    books[i].name = req.body.name;
+                    books[i].price = req.body.price;
+                }
+            }
+
+            res.status(204);
+            res.send();
+        }
+    }
+
+    patch(req, res) {
+        const id = +req.params.id;
+        delete req.body.id;
+        const body = req.body;
+
+        // reflection
+        for (let i = 0; i < books.length; i++) {
+            if (books[i].id === id) {
+                const book = books[i];
+                for (let key in body) {
+                    book[key] = body[key];
+                }
+            }
+        }
+
+        res.status(204);
+        res.send();
     }
 }
 
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const bodyParser = require('body-parser')
 const defaultCtrl = require('./controllers/defaultCtrl');
 const bookCtrl = require('./controllers/bookCtrl');
 const authorCtrl = require('./controllers/authorCtrl');
@@ -9,11 +10,17 @@ app.listen(3000, function () {
     console.log("Server is running on port 3000");
 });
 
+app.use(bodyParser.json());
+
 // routing
 app.get('/', defaultCtrl.home);
 app.get('/health', defaultCtrl.health);
 
 app.get('/books', bookCtrl.get);
-app.get('/books/:id',bookCtrl.getById);
-app.post('/books',bookCtrl.create);
+app.get('/books/:id', bookCtrl.getById);
+app.post('/books', bookCtrl.create);
+app.delete('/books/:id', bookCtrl.delete);
+app.put('/books/:id', bookCtrl.update);
+app.patch('/books/:id', bookCtrl.patch);
+
 app.get('/authors', authorCtrl.get);
