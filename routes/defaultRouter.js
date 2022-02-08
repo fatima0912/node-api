87bba4420ddf34c4390b16f646b5558a0812d548
Mujib <mujib517@gmail.com>
2022-02-08 08:13:59 +0530

    clean up

--- routes/defaultRouter.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const defaultCtrl = require('../controllers/defaultCtrl');
-const router = express.Router();
-
-router.get('/', defaultCtrl.home);
-router.get('/health', defaultCtrl.health);
-
-module.exports = router;
