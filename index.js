43f6d2dfccd64452171c2bb9dffb9005e856f6ce
Mujib <mujib517@gmail.com>
2022-02-08 09:02:12 +0530

    Health endpoint

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const config = require('./config/index');
 const deafultRouter = require('./routes/defaultRouter');
+
 const app = express();
 const port = 3000;
 
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
+
+mongoose.connect(config.dbConStr)
+    .then(res => console.log('Connected to MongoDb'))
+    .catch(err => console.log('failed to connect to db'));
 
 app.use('/', deafultRouter);
